c760406fa614d42122c66a2fe1611cb4ec12e685
Marchons <d1286360646@outlook.com>
2023-04-02 11:23:27 +0800

    feat: 添加了core模块，用于编写具体代码分析算法，api模块增加了BaseResponse用于收敛返回内容以及controller中提供ast,cfg,pdg,ddg接口

---
+++ api/src/main/java/com/scut/mplas/util/ResponseBuilder.java
@@ -0,0 +1,31 @@
+package com.scut.mplas.util;
+
+import com.scut.mplas.common.response.BaseResponse;
+import com.scut.mplas.common.response.ResponseCode;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResponseBuilder {
+
+    /**
+     * @return  generate a success response without data
+     */
+    public BaseResponse getSuccessResponse(){
+        return new BaseResponse(ResponseCode.SUCCESS);
+    }
+
+    /**
+     * @param data data include:ast, cfg, pdg, ddg etc
+     * @return generate a success response with data
+     */
+    public BaseResponse getSuccessResponse(Object data){
+        return new BaseResponse(ResponseCode.SUCCESS, data);
+    }
+
+    /**
+     * @return generate a fail response with data
+     */
+    public BaseResponse getFailResponse(){
+        return new BaseResponse(ResponseCode.FAIL);
+    }
+}
