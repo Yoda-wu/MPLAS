629b9725ec5f08830991a8945d670cf768358d2f
张家怡 <649503913@qq.com>
2023-04-21 23:05:20 +0800

    完成goto前置跳转和后置跳转的实现和检测 cfg检测全部通过

--- core/src/test/resources/cpp/basic/goto_test.cpp
+++ core/src/test/resources/cpp/basic/goto_test.cpp
@@ -4,12 +4,15 @@ int main(int argc, const char * argv[]) {
 
     cout<<"1111111"<<endl;
     cout<<"2222222"<<endl;
-// EXIT:
+    //GOTO 跳转到前置label
+    LABEL2:
         cout<<"5555555"<<endl;
+        //goto跳转到后置label
     goto EXIT;
     cout<<"3333333"<<endl;
     EXIT:
     cout<<"4444444"<<endl;
+    goto LABEL2;
 
     return 0;
 }
