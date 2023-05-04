7aaaa63ecf2b2dfd6f2fe8a72cfe3e0097b618d2
aliyun4384871743 <1253576153@qq.com>
2023-05-04 15:37:19 +0800

    feat:增加源代码文件转为jsonl文件的函数

--- core/src/test/java/com/scut/mplas/java/BasicAnalysisTests.java
+++ core/src/test/java/com/scut/mplas/java/BasicAnalysisTests.java
@@ -2,7 +2,18 @@
 package com.scut.mplas.java;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scut.mplas.Execution;
 import com.scut.mplas.Main;
 import com.scut.mplas.utils.FileUtils;
@@ -47,6 +58,79 @@ public class BasicAnalysisTests {
             }
         }
         dir.delete();
+    }
+
+    // 将指定的目录下的所有代码文件，转换为一个jsonl，每一个源代码文件对应生成的jsonl文件中的一条json对象
+    // 每一个代码文件都会转换为一个json对象,json对象格式如下：
+    // { test：“文件名”，code：“源代码文件里的文本”}
+    private static final String CPP_JSONL_DIR="src/test/resources/cpp/testJSONL/";
+    private static final String CPP_SUFFIX=".cpp";
+
+    private static final String JS_JSONL_DIR="src/test/resources/js/testJSONL/";
+    private static final String JS_SUFFIX=".js";
+
+    private final String JAVA_JSONL_DIR = "src/test/resources/java/testJSONL/";
+    private final String JAVA_SUFFIX=".java";
+
+    @Test
+    public void produceJSONL() throws IOException {
+        // 源代码文件所在目录
+        String inputDir="";
+        // 生成的jsonl文件输出目录
+        String outDir="";
+        // 指定源代码文件的后缀名
+        String suffix="";
+
+        //String lang="cpp";
+        //String lang="java";
+        String lang="js";
+        //String lang="ruby";
+        switch(lang)
+        {
+            case "cpp":
+                inputDir=CPP_SRC_DIR;
+                outDir=CPP_JSONL_DIR;
+                suffix=CPP_SUFFIX;
+                break;
+            case "java":
+                inputDir=JAVA_SRC_DIR;
+                outDir=JAVA_JSONL_DIR;
+                suffix=JAVA_SUFFIX;
+                break;
+            case "js":
+                inputDir=JS_SRC_DIR;
+                outDir=JS_JSONL_DIR;
+                suffix=JS_SUFFIX;
+                break;
+            case "ruby":
+                break;
+            default:
+                return;
+        }
+
+        String[] filePaths=new String[0];
+        filePaths=FileUtils.listFilesWithSuffix(new String[]{inputDir},suffix);
+
+        String outputName=outDir+lang+"Test.jsonl";
+        File outputFile=new File(outputName);
+        FileWriter writer=new FileWriter(outputFile);
+
+        for(String srcFile:filePaths)
+        {
+            File f=new File(srcFile);
+            Map<String,String> srcJson=new HashMap<>();
+            Path path = Paths.get(srcFile);
+            String content = Files.readString(path, StandardCharsets.UTF_8);
+            srcJson.put("text",f.getName());
+            srcJson.put("code",content);
+
+            // 创建 ObjectMapper 对象
+            ObjectMapper mapper = new ObjectMapper();
+            // 将 Map 对象转换为 JSON 字符串
+            String json = mapper.writeValueAsString(srcJson);
+            writer.write(json+"\n");
+        }
+        writer.close();
     }
     
     @Test
